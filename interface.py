f074a7612a39f31717311d1118dc484f93ccd703
Melkor <rintfd@163.com>
2022-09-09 09:33:23 +0800

    Update interface.py

--- interface.py
+++ interface.py
@@ -1,7 +1,8 @@
 from melkor_parser.model import *
 
 corpus = ["I am melkor",
-          "I am mal ganis"]
+          "I am mal ganis",
+          "cross a 3 and add the 2"]
 cfg_diction = {"+":{"output_type":"int","input_types":["int","int"]},
                "1":{"output_type":"int","input_types":None},
                "2":{"output_type":"int","input_types":None}}
@@ -14,17 +15,22 @@ def make_melkor_parser(corpus,cfg_diction,word_dim=256,signal_dim=32,key_dim=42,
 
 model = make_melkor_parser(corpus,cfg_diction,signal_dim=132,key_dim=44)
 
+data = [
+    ["I am mal ganis",["+","1","+","2","1"]],
+    ["I am melkor",["+","1","2"]],
+    ["cross a 3 and add the 2",["+","+","2","1","+","1","2"]],
+]
+
 optim = torch.optim.Adam(model.parameters(),lr=2e-3)
 for epoch in range(100):
     optim.zero_grad()
-    p,l = model("I am mal ganis",["+","+","1","2","+","2","1"])
-    p2,l2 = model("I am melkor",["+","1","2"])
-    l = l + l2
+    l = 0
+    for bind in data:
+        p,l2 = model(bind[0],bind[1])
+        l += l2
     l.backward()
     optim.step()
 
 model.monte_carlo_enabled = False
-p,l = model("I am mal ganis")
-print(p,l)
-p,l = model("I am melkor")
-print(p,l)
+for bind in data:
+    print(model(bind[0])[0])
